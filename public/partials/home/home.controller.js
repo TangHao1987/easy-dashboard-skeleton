2622bfb13479ff84a32d56168f89f6662624094b
TangHao1987 <mythabc1987@gmail.com>
2015-07-25 23:03:48 +0800

    1) implemented filter for the menu 2) change the button to icon inside input text box

--- public/partials/home/home.controller.js
+++ public/partials/home/home.controller.js
@@ -34,4 +34,39 @@
             }
         };
     }]);
+
+    controllers.filter('menuFilter', function($filter){
+        var standardFilter = $filter('filter');
+        return function(menuItems , searchText){
+            var out = standardFilter(menuItems, searchText);
+            if(searchText && searchText.length > 0) {
+                _.each(out, function (menuItem) {
+                    if (menuItem.subItems != undefined) {
+                        var subItem = _.find(menuItem.subItems, function (subItem) {
+                            return subItem.name.match(new RegExp(searchText,"i"));
+                        });
+
+                        var findSub = false;
+                        _.each(menuItem.subItems, function (subItem) {
+                            if(subItem.subItems){
+                                var subSubItem = _.find(subItem.subItems, function (subSubItem) {
+                                    return subSubItem.name.match(new RegExp(searchText,"i"));
+                                });
+                                if(subSubItem !== undefined){
+                                    subItem.showSub = true;
+                                    findSub = true;
+                                }else{
+                                    subItem.showSub = false;
+                                }
+                            }
+                        });
+
+                        menuItem.showSub = !!(subItem !== undefined || findSub);
+                    }
+
+                });
+            }
+            return out;
+        }
+    })
 })();
