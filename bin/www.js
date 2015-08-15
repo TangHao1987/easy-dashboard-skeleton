442013b5fd6b6d66505e8e5b4c0591a342281312
TangHao1987 <mythabc1987@gmail.com>
2015-08-15 22:19:26 +0800

    added the new readme file,  refractored the project name

--- bin/www.js
+++ bin/www.js
@@ -5,7 +5,7 @@
  */
 
 var app = require('../app');
-var debug = require('debug')('currencyStreet:server');
+var debug = require('debug')('easy-dashboard-skeleton:server');
 var http = require('http');
 
 /**
